919be695837922f2fcfc3e0a57e318d97662c23c
kamcioramcio <kamilecki12@gmail.comgit config --global user.email kamilecki12@gmail.com>
2024-04-30 13:20:59 +0200

    Khan-Tarjan-DONE

--- graph.py
+++ graph.py
@@ -3,7 +3,8 @@ import queue
 
 
 
-# jakiś mądry człowiek powiedział, że naukowcy mówią edges a nie verticles pozdro
+
+# jakiś mądry człowiek powiedział, że naukowcy mówią nodes a nie verticles pozdro
 class Graph:
     def __init__(self, nodes):
         self.nodes = nodes
@@ -84,23 +85,121 @@ class Graph:
                     order += self.DFS(neighbour, visited)
         return order
     # Algorytm Khana
+    # Wazne: degree sie liczy, bo powino byc tak ze zaczynasz od node który ma degree 0 i idziej rosnaco
+    def degree(self, node):
+        return sum(1 for edges in self.adj_list.values() if node in edges)
     
-    
-    
+    def Khan_Algorithm(self):
+        # w naszym przypadku nie ma znaczenia czy to indegree czy outdegree bo jest to graf skierowany na 100%
+        in_degree ={n: 0 for n in self.adj_list}
+        for n in self.adj_list:
+            for neighbour in self.adj_list[n]:
+                in_degree[neighbour] += 1
+        
+        q = queue.Queue()
+        for n in in_degree:
+            if in_degree[n] == 0:
+                q.put(n)
+        
+        order = []
+        while not q.empty():
+            node = q.get()
+            order.append(node)
+            
+            for neighbour in self.adj_list[node]:
+                in_degree[neighbour] -= 1
+                if in_degree[neighbour] == 0:
+                    q.put(neighbour)
+        
+        # mozna jeszcze sprawcic czy nie ma cyklu w grafie nizej warunek
+        if len(order) != len(self.adj_list):
+            return print("Cykl w grafie")
+        
+        else:
+            return order
+        
     
     # Algorytm Tarjana
-    
-    
+    # Ale to jest dym brachu
+    # https://www.youtube.com/watch?v=_1TDxihjtoE spoko wytłumaczone
+    #
+    def Trajan_Algorithm(self):
+        # Po ludzku wytłumacze bazowo kazdy wierzchołek ma index -1 ,sygnalizuje, że dla danego wierzchołka nie ustalono jescze indexu
+        index = {n: -1 for n in self.adj_list}
+        # Jest to wartość, która będzie reprezentować najniższy numer porządkowy w komponencie silnie spójnej dla danego wierzchołka.
+        low_link = {n: -1 for n in self.adj_list}
+        # Info czy dany wierzchołek jest na stosie czy nie
+        on_stack = {n: False for n in self.adj_list}
+
+        stack = []
+        order = []
+        
+        # jak nie dałem tego w liste to nie działało 
+        index_counter = [0]
+        
+        def strong_connect(node):
+            index[node] = index_counter[0]
+            low_link[node] = index_counter[0]
+            index_counter[0] += 1
+            stack.append(node)
+            on_stack[node] = True
             
-if __name__ == "__main__":
-    nodes = 10  # specify the number of nodes
-    saturation = 0.5  # specify the saturation
-    g = Graph(nodes)
-    g.generate(saturation)
-#   g.table()
-#   g.matrix()
-    print("Adjacency list:", g.adj_list)
-    print(g.BFS(0))
-    print(g.DFS(0))
-    
-    
+            for neighbour in self.adj_list[node]:
+                if index[neighbour] == -1:
+                    strong_connect(neighbour)
+                    low_link[node] = min(low_link[node], low_link[neighbour])
+                elif on_stack[neighbour]:
+                    low_link[node] = min(low_link[node], index[neighbour])
+            
+            if low_link[node] == index[node]:
+                component = []
+                while True:
+                    neighbour = stack.pop()
+                    on_stack[neighbour] = False
+                    component.append(neighbour)
+                    if neighbour == node:
+                        break
+                order.extend(component)
+            
+        for node in self.adj_list:
+            if index[node] == -1:
+                strong_connect(node)
+        return order
+      
+    def to_latex(self, filename):
+        with open(filename, 'w') as f:
+            f.write("\\documentclass{article}\n")
+            f.write("\\usepackage{tikz}\n")
+            f.write("\\begin{document}\n")
+            f.write("\\begin{figure}\n")
+            f.write("\\centering\n")
+            f.write("\\begin{tikzpicture}[auto, node distance=2cm, every loop/.style={},]\n")
+            for node in self.adj_list:
+                # zamiast circle mona dawać rózne figury
+                f.write(f"\\node[draw, circle] ({node}) at ({node * 360/self.nodes}:3cm) {{$ {node} $}};\n")
+            for node, edges in self.adj_list.items():
+                for neighbour in edges:
+                    if neighbour != node:  
+                        f.write(f"\\path[->] ({node}) edge node {{}} ({neighbour});\n")
+            f.write("\\end{tikzpicture}\n")
+            f.write("\\caption{Directed Graph}\n")
+            f.write("\\end{figure}\n")
+            f.write("\\end{document}\n")
+            
+#if __name__ == "__main__":
+#    nodes = 10  # specify the number of nodes
+#    saturation = 0.5  # specify the saturation
+#    g = Graph(nodes)
+#    g.generate(saturation)
+#    g.table()
+#    g.matrix()
+#    print("Adjacency list:", g.adj_list)
+#    print(g.BFS(0))
+#    print(g.DFS(0))
+#    print(g.degree(0))
+#    print(g.Khan_Algorithm())
+#    print(g.Trajan_Algorithm())
+#    g.to_latex('graph.tex')
+
+
+     
